ffffcf383289c5f7f14fd69b94763f25fee884f2
Mohamed Gamal <mg.dev1992@icloud.com>
2023-04-12 03:49:00 +0200

    set return type

--- src/Facades/Moyasar.php
+++ src/Facades/Moyasar.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Facade;
  */
 class Moyasar extends Facade
 {
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return \MG\Moyasar\Moyasar::class;
     }
