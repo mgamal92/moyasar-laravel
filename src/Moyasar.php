083525b4d13feb30ac6d8ef82627415e93af83d9
Mohamed Gamal <mg.dev1992@icloud.com>
2023-04-12 14:15:59 +0200

    init moyasar class

--- src/Moyasar.php
+++ src/Moyasar.php
@@ -2,27 +2,15 @@
 
 namespace MG\Moyasar;
 
-use Illuminate\Support\Facades\Http;
-
 class Moyasar
 {
-    public function pay()
+    const BASE_URL = 'https://api.moyasar.com/v1/';
+
+    private function headers(): array
     {
-        return Http::asForm()->withHeaders([
+        return [
             'Authorization' => 'Basic '.base64_encode(config('moyasar-laravel.secret_key')),
             'Content-Type' => 'application/x-www-form-urlencoded',
-        ])->post('https://api.moyasar.com/v1/payments', [
-            'amount' => 1000,
-            'source' => [
-                'company' => 'visa',
-                'type' => 'creditcard',
-                'name' => 'name',
-                'number' => '4111111111111111',
-                'cvc' => '123',
-                'month' => '03',
-                'year' => '30',
-            ],
-            'callback_url' => 'https://test.com',
-        ]);
+        ];
     }
 }
