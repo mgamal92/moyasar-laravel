8fed35b44aedbb25575fc02585ed74ac8a983c76
Mohamed Gamal <mg.dev1992@icloud.com>
2023-04-13 02:09:18 +0200

    init creating payment

--- src/Moyasar.php
+++ src/Moyasar.php
@@ -2,6 +2,30 @@
 
 namespace MG\Moyasar;
 
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Client\Response;
+
 class Moyasar
 {
+    public const BASE_URL = 'https://api.moyasar.com/v1/';
+
+    private function headers(): array
+    {
+        return [
+            'Authorization' => 'Basic ' . base64_encode(config('moyasar-laravel.secret_key')),
+            'Content-Type' => 'application/x-www-form-urlencoded',
+        ];
+    }
+
+    public function initiate($data): Response
+    {
+        try {
+            return Http::asForm()
+                ->withHeaders($this->headers())
+                ->post(self::BASE_URL . 'payments', $data);
+        } catch (\Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+
 }
---
+++ tests/Unit/MoyasarTest.php
@@ -0,0 +1,52 @@
+<?php
+
+use MG\Moyasar\Moyasar;
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\HttpFoundation\Response;
+
+it('test initiating a new payment', function () {
+    Http::fake([
+        '*/payments' => Http::response(["status" => "initiated", Response::HTTP_CREATED])
+    ]);
+
+    $response = (new Moyasar())->initiate([
+        'amount' => 1000,
+        'source' => [
+            'company' => 'visa',
+            'type' => 'creditcard',
+            'name' => 'name',
+            'number' => '411111111111',
+            'cvc' => '123',
+            'month' => '03',
+            'year' => '30',
+        ],
+        'callback_url' => 'https://test.com',
+    ]);
+
+    expect($response->json())->toHaveKeys(['status']);
+
+});
+
+
+it('test initiating a new payment with wrong data', function () {
+    Http::fake([
+        '*/payments' => Http::response(['message' => 'Validation Failed'], Response::HTTP_BAD_REQUEST)
+    ]);
+
+    $response = (new Moyasar())->initiate([
+        'amount' => 1000,
+        'source' => [
+            'company' => 'visa',
+            'type' => 'creditcard',
+            'name' => 'name',
+            'number' => '1010101010',
+            'cvc' => '123',
+            'month' => '03',
+            'year' => '30',
+        ],
+        'callback_url' => 'https://test.com',
+    ]);
+
+    expect($response->json())->toHaveKeys(['message']);
+
+});
